195d156b50f9e81b7e6df00c655fe51b13f3c5e3
anskalecka <ania.skalka@interia.pl>
2017-02-28 18:24:32 +0100

    make data base connection nice-looking

--- Aplikacja1/src/javaee/other/DatabaseConnector.java
+++ Aplikacja1/src/javaee/other/DatabaseConnector.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -15,95 +16,72 @@ import java.util.Scanner;
 
 public class DatabaseConnector {
 
-	public String connect(String path) {
-
-		// wyczytanie z pliku + zapisanie konfiguracji
+	private void setProperties(String path) throws IOException, FileNotFoundException{
 		Properties prop = new Properties();
 		Scanner in = null;
-		try {
+		
+		in = new Scanner(new File(path));
 
-			in = new Scanner(new File(path));
+		prop.setProperty("database", in.nextLine());
+		prop.setProperty("dbuser", in.nextLine());
+		prop.setProperty("dbpassword", in.nextLine());
 
-			prop.setProperty("database", in.nextLine());
-			prop.setProperty("dbuser", in.nextLine());
-			prop.setProperty("dbpassword", in.nextLine());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
+		try{
+			in.close();
+		} catch (Exception e){
+			e.printStackTrace();			
 		}
 
 		OutputStream output = null;
-		try {
-			output = new FileOutputStream("config.properties");
-			prop.store(output, null);
-		} catch (IOException io) {
-			io.printStackTrace();
-		} finally {
-			if (output != null) {
-				try {
-					output.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+		output = new FileOutputStream("config.properties");
+		prop.store(output, null);
+}
+	
+	
+	public Connection getConnection(String path) throws FileNotFoundException, IOException {
+
+		setProperties(path);
 
 		// korzystanie z config.properties w celu polaczenia z baza
-		prop = new Properties();
+		Properties prop = new Properties();
 		InputStream input = null;
 		String database;
 		String url = null;
 		String username = null;
 		String password = null;
 
-		try {
+		input = new FileInputStream("config.properties");
 
-			input = new FileInputStream("config.properties");
+		// load a properties file
+		prop.load(input);
 
-			// load a properties file
-			prop.load(input);
+		// get the property value
+		database = prop.getProperty("database");
+		url = "jdbc:mysql://localhost:3306/" + database;
+		username = prop.getProperty("dbuser");
+		password = prop.getProperty("dbpassword");
 
-			// get the property value
-			database = prop.getProperty("database");
-			url = "jdbc:mysql://localhost:3306/" + database;
-			username = prop.getProperty("dbuser");
-			password = prop.getProperty("dbpassword");
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+		loadDriver();
+		
+		System.out.println("Connecting database...");
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Database connected!");
+		} catch (SQLException e) {
+			System.out.println("Cannot connect the database!");
+			throw new IllegalStateException("Cannot connect the database!", e);
 		}
-
+		return connection;
+	}
+	
+	private void loadDriver(){
 		System.out.println("Loading driver...");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver loaded!");
 		} catch (ClassNotFoundException e) {
 			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
-		}
-
-		System.out.println("Connecting database...");
-		try (Connection connection = DriverManager.getConnection(url, username, password)) {
-			System.out.println("Database connected!");
-		} catch (SQLException e) {
-			System.out.println("Cannot connect the database!");
-			throw new IllegalStateException("Cannot connect the database!", e);
-		}
-
-		return url + "?user=" + username + "&password=" + password;
+		}		
 	}
 }
--- Learn/Servlets/Servlet-connectionToDatabase.java
+++ Learn/Servlets/Servlet-connectionToDatabase.java
@@ -83,3 +83,95 @@ public class WitajSwiecieServlet extends HttpServlet {
 	}
 
 }
+
+
+//--------------------
+
+
+package javaee.other;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class DatabaseConnector {
+
+	private void setProperties(String path) throws IOException, FileNotFoundException{
+		Properties prop = new Properties();
+		Scanner in = null;
+		
+		in = new Scanner(new File(path));
+
+		prop.setProperty("database", in.nextLine());
+		prop.setProperty("dbuser", in.nextLine());
+		prop.setProperty("dbpassword", in.nextLine());
+
+		try{
+			in.close();
+		} catch (Exception e){
+			e.printStackTrace();			
+		}
+
+		OutputStream output = null;
+		output = new FileOutputStream("config.properties");
+		prop.store(output, null);
+}
+	
+	
+	public Connection getConnection(String path) throws FileNotFoundException, IOException {
+
+		setProperties(path);
+
+		// korzystanie z config.properties w celu polaczenia z baza
+		Properties prop = new Properties();
+		InputStream input = null;
+		String database;
+		String url = null;
+		String username = null;
+		String password = null;
+
+		input = new FileInputStream("config.properties");
+
+		// load a properties file
+		prop.load(input);
+
+		// get the property value
+		database = prop.getProperty("database");
+		url = "jdbc:mysql://localhost:3306/" + database;
+		username = prop.getProperty("dbuser");
+		password = prop.getProperty("dbpassword");
+
+		loadDriver();
+		
+		System.out.println("Connecting database...");
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Database connected!");
+		} catch (SQLException e) {
+			System.out.println("Cannot connect the database!");
+			throw new IllegalStateException("Cannot connect the database!", e);
+		}
+		return connection;
+	}
+	
+	private void loadDriver(){
+		System.out.println("Loading driver...");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+		}		
+	}
+}
